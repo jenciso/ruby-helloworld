b17bed9b20cbbf6d3fe1c7368ec5a20d17295834
Juan Enciso <juan.enciso@gmail.com>
2022-01-09 22:36:52 -0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -6,18 +6,13 @@ configure {
 }                                
                                  
 class App < Sinatra::Base
-
   get '/' do
     "Hello World with Ruby!"
   end
-
   get '/hi' do
     "Hi!"
   end
-
-
-  get '/health' do                         
-    "Im Alive, TimeStamp: #{Time.now}"     
-  end                                      
- 
+  get '/health' do     
+    "Im Alive, TimeStamp: #{Time.now}" 
+  end
 end
